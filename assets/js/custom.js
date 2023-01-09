ff84dfcb5b3e0872e4982c96c84223cb2f51cf52
Aminul721 <aminul721@gmail.com>
2023-01-09 14:52:23 +0600

    added all files

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+
+new WOW().init(); 
+
+jQuery(document).ready(function() {
+    jQuery('#menu').slicknav({
+        prependTo:'#menu-holder',
+        label: '',
+        closedSymbol: '',
+        openedSymbol: ''
+    });
+});
